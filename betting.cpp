f2a73d844c6ac3d8e521e14f6d4a8e82bcbe0ac2
ghazalrb98 <ghazalrb.98@gmail.com>
2022-10-25 15:32:16 +0200

    solutions

---
+++ betting.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  float n;
+  cin >> n;
+  cout << fixed << setprecision(3);
+  cout << 100 / n << endl;
+  cout << (100) / (100 - n) << endl;
+}
---
+++ echoechoecho.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+  cout << s;
+  for (int i = 0; i < 2; i++) {
+    cout << " " << s;
+  }
+  cout << endl;
+}
